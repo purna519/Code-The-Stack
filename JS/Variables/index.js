d70f6ba335743de02622c46333dbc9d6a4cd0acc
Purna Chandra Rao <dhanekula591@gmail.com>
2025-09-12 16:41:27 +0530

    JS | Intro | Variables

---
+++ JS/Variables/index.js
@@ -0,0 +1,17 @@
+let a;
+a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+a = 30;
+console.log(a);
+
+var b = 10;
+console.log(b);
+b = 30;
+console.log(b);
+var b = 40;
+console.log(b);
+
+const fact = "Sun always rises in the east";
+console.log(fact);
