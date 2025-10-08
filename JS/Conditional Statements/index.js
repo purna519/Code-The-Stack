148c51ea33a180ac7e1ab33d49a5d18397d61889
Purna Chandra Rao <dhanekula591@gmail.com>
2025-10-08 19:55:22 +0530

    BillAmount

--- JS/Conditional Statements/index.js
+++ JS/Conditional Statements/index.js
@@ -26,64 +26,64 @@
 
 // if bill amount is 1k above then computer should print bill amount reduction 10% , if 2k above then reduction of 20% , if 4k above 50%
 
-// billAmount = 2567
+billAmount = prompt("Enter bill Amount : ");
 
-// if (billAmount > 1000) {
-//     billAmount = billAmount * 0.1
-//     console.log(billAmount);
-// }
-// else if (billAmount > 2000) {
-//     billAmount = billAmount * 0.2;
-//     console.log(billAmount);
-// }
-// else if (billAmount > 4000) {
-//     billAmount = billAmount * 0.5;
-//     console.log(billAmount);
-// }
-// else {
-//     console.log("Sorry you didnt get any discount");
-// }
+if (billAmount >= 1000 && billAmount < 2000) {
+    billAmount = billAmount * 0.1
+    console.log(billAmount);
+}
+else if (billAmount >= 2000 && billAmount < 4000) {
+    billAmount = billAmount * 0.2;
+    console.log(billAmount);
+}
+else if (billAmount > 4000) {
+    billAmount = billAmount * 0.5;
+    console.log(billAmount);
+}
+else {
+    console.log("Sorry you didnt get any discount");
+}
 
 
 // Nested if
 // =========
 
-Kajal = 0
-SaiPallavi = 0
-Tammana = -3/4
+// Kajal = 0
+// SaiPallavi = 0
+// Tammana = -3/4
 
 
-if (Kajal != SaiPallavi) {
-    if (Kajal != Tammana) {
-        if (Kajal > SaiPallavi) {
-            if (Kajal > Tammana) {
-                console.log("Kajal is topper");
-            }
-            else {
-                console.log("Tammana is topper");
-            }
-        }
-        else if (SaiPallavi > Tammana) {
-            console.log("Sai pallavi is topper");
-        }
-        else {
-            console.log("Tammana is topper");
-        }
-    }
-    else if (Kajal > SaiPallavi) {
-        console.log("kajal and tammana are the toppers");
-    }
-    else {
-        console.log("sai pallavi is the topper");
-    }
-}
-else if (Kajal == Tammana) {
-    console.log("All are topppers");
-}
-else if (Tammana > Kajal) {
-    console.log("Tammana is topper");
-}
-else {
+// if (Kajal != SaiPallavi) {
+//     if (Kajal != Tammana) {
+//         if (Kajal > SaiPallavi) {
+//             if (Kajal > Tammana) {
+//                 console.log("Kajal is topper");
+//             }
+//             else {
+//                 console.log("Tammana is topper");
+//             }
+//         }
+//         else if (SaiPallavi > Tammana) {
+//             console.log("Sai pallavi is topper");
+//         }
+//         else {
+//             console.log("Tammana is topper");
+//         }
+//     }
+//     else if (Kajal > SaiPallavi) {
+//         console.log("kajal and tammana are the toppers");
+//     }
+//     else {
+//         console.log("sai pallavi is the topper");
+//     }
+// }
+// else if (Kajal == Tammana) {
+//     console.log("All are topppers");
+// }
+// else if (Tammana > Kajal) {
+//     console.log("Tammana is topper");
+// }
+// else {
     // if (Kajal < 0) {
     //     if (SaiPallavi < 0) {
     //         if (Tammana < 0) {
@@ -105,5 +105,5 @@ else {
     //         }
     //     }
     // }
-    console.log("Invalid data");
-}
+//     console.log("Invalid data");
+// }
