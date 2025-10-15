709bd9c05ef56e7b45c08e31f6fcdf7eb27f82a1
Purna Chandra Rao <dhanekula591@gmail.com>
2025-10-15 23:00:52 +0530

    Nested Loops

---
+++ JS/Loops/index.js
@@ -0,0 +1,89 @@
+// while loop
+// ==========
+
+// let i = 1;
+
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// do while loop
+// ==============
+
+// do {
+//     console.log(i);
+    
+// } while (i < 1);
+
+
+// For loop
+// ==========
+
+// for (i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+
+// Switch case
+// ===========
+// let a = 10;
+
+// switch (a) {
+//     case 1:
+//         console.log("the value is 1");
+//         break;
+//     case 2:
+//         console.log("the value is 10");
+//         break;
+//     case 3:
+//         console.log("the value is 3");
+//         break;
+//     default:
+//         console.log("Your entered numbe is not in case values");
+// }
+
+
+// Counitnue
+// =========
+// for (i = 0; i < 11; i++) {
+//     if (i == 5) {
+//         continue;
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+// let i = 1;
+// j = 1;
+// while (i <= 4) {
+//     console.log("£");
+//     while (j <= i) {
+//         console.log("*");
+//         j++;
+//     }
+//     i++;
+// }
+
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// for (let row = 1; row <= 5; row++) {
+//     for (let column = 1; column <= row; column++) {
+//         console.log("* ");
+//     }
+//     console.log("");
+    
+// }
+
+// "*" ==> String
+// for (let row = 1; row <= 5; row++) {
+//     console.log("* ".repeat(row));
+// }
