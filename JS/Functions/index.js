09bc4f96efd315d8010a0cf03fa54f0e88d57ed3
Purna Chandra Rao <dhanekula591@gmail.com>
2025-10-18 17:51:05 +0530

    Functions

---
+++ JS/Functions/index.js
@@ -0,0 +1,82 @@
+// Normal / Named Function
+// ==========================
+
+// function Marriage() {
+//     console.log("Bride & Groom");
+// }
+
+// Marriage();
+
+
+// Function With Parameters
+// ========================
+
+// function Marriage(Bride,Groom) {
+//     console.log(Bride +" Weds "+ Groom);
+// }
+
+// Marriage("Purna", "Pallu");
+
+
+// Marriage("John", "Sweety");
+
+// =========================================================================================================================================================
+
+// Anonymous Function
+// ======================
+
+// purna = function () {
+//     console.log("Purna is a good boy");
+// }
+
+// purna();
+
+
+
+// ============================================================================================================================================================
+
+// Higher Order Function
+// =====================
+
+// when i called a function it will give return statemtn or function.
+// When a function is taking another function as argument
+
+// function a(aVariable) {
+//     return aVariable;
+// }
+
+// let result = a(10);
+// console.log(result);
+
+// function b(a) {
+//     return a+20
+// }
+
+// let result = b(a(10));
+// console.log(result);
+
+// function namee() {
+//     namee()
+// }
+
+// ==============================================================================================================================================================
+
+
+
+// Arrow Function
+// ================
+
+// let b = () => {
+//     console.log("Hi all");
+    
+// }
+
+// b();
+
+
+
+
+
+// Task : By using functions crate a table() function 
+// table(5);
+// table(10);
